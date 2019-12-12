56a3ffc0657173c2d760037fc616f7b5add347f3
Glitch (storybook-react-schema) <none>
2019-12-12 07:36:43 +0000

    from glitch

---
+++ storybook-static/precache-manifest.327efdcae18b120d03b6d76e1aa7bec3.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adb9627b214760a54933517d2187ba4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.119e1689a0803d31ef47.bundle.js"
+  },
+  {
+    "url": "runtime~main.119e1689a0803d31ef47.bundle.js"
+  },
+  {
+    "url": "vendors~main.119e1689a0803d31ef47.bundle.js"
+  }
+]);
